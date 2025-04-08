380ca1724f51dff027053e3f95240ab5b07f9c82
muath-gh <gharablipro2017@gmail.com>
2025-04-08 08:07:47 +0300

    comments

--- resources/views/livewire/comments.blade.php
+++ resources/views/livewire/comments.blade.php
@@ -1,6 +1,6 @@
 <div>
     <h1 class="font-bold tracking-widest text-center text-black uppercase">
-        {{ trans_choice(':count comment|:count comments', $commentsCount) }}
+        {{ trans_choice(':count تعليق|:count تعليقات', $commentsCount) }}
     </h1>
 
     <div class="grid gap-8 mt-8">
