f1d3d17ffd7483c47653e8a25501c6763a648b98
muath-gh <gharablipro2017@gmail.com>
2025-04-09 10:29:32 +0300

    fav

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -10,7 +10,14 @@
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
-
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="48x48" href="{{ asset('favicon/favicon-48x48.png') }}">
+        <link rel="icon" type="image/png" sizes="64x64" href="{{ asset('favicon/favicon-64x64.png') }}">
+        <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('favicon/favicon-96x96.png') }}">
+        <link rel="icon" type="image/png" sizes="128x128" href="{{ asset('favicon/favicon-128x128.png') }}">
+        <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('favicon/favicon-192x192.png') }}">
+        <link rel="icon" type="image/png" sizes="256x256" href="{{ asset('favicon/favicon-256x256.png') }}">
         <title>{{ $title }}</title>
 
         <meta name="title" content="{{ $title }}" />
